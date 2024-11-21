375a0a8bb93fc93c2159d00f27a12463061f18b5
mwatermelon <m.watermelon20@gmail.com>
2024-11-21 08:09:46 -0600

    awesome changes

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 
-let draggableElement = document.getElementById('draggableElement');
-let offsetX, offsetY;
+// let draggableElement = document.getElementById('draggableElement');
+// let offsetX, offsetY;
 
-draggableElement.addEventListener('mousedown', startDragging);
-draggableElement.addEventListener('mouseup', stopDragging);
+// draggableElement.addEventListener('mousedown', startDragging);
+// draggableElement.addEventListener('mouseup', stopDragging);
 
-function startDragging(e) {
-    e.preventDefault();
-    offsetX = e.clientX - draggableElement.getBoundingClientRect().left;
-    offsetY = e.clientY - draggableElement.getBoundingClientRect().top;
-    draggableElement.classList.add('dragging');
-    document.addEventListener('mousemove', dragElement);
-}
+// function startDragging(e) {
+//     e.preventDefault();
+//     offsetX = e.clientX - draggableElement.getBoundingClientRect().left;
+//     offsetY = e.clientY - draggableElement.getBoundingClientRect().top;
+//     draggableElement.classList.add('dragging');
+//     document.addEventListener('mousemove', dragElement);
+// }
 
-function dragElement(e) {
-    e.preventDefault();
-    let x = e.clientX - offsetX;
-    let y = e.clientY - offsetY;
-    draggableElement.style.left = x + 'px';
-    draggableElement.style.top = y + 'px';
-}
+// function dragElement(e) {
+//     e.preventDefault();
+//     let x = e.clientX - offsetX;
+//     let y = e.clientY - offsetY;
+//     draggableElement.style.left = x + 'px';
+//     draggableElement.style.top = y + 'px';
+// }
 
-function stopDragging() {
-    draggableElement.classList.remove('dragging');
-    document.removeEventListener('mousemove', dragElement);
+// function stopDragging() {
+//     draggableElement.classList.remove('dragging');
+//     document.removeEventListener('mousemove', dragElement);
+// }
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 300) {
+    /*
+    document.getElementById("navbar").style.width = "150px";
+   
+    */
+    document.getElementById("navbar").style.position = "fixed";
+    document.getElementById("navbar").style.background="linear-gradient(60deg,rgb(233, 99, 157) 0%, rgba(148,187,233,1) 100%)";
+
+  } else {
+    document.getElementById("navbar").style.position = "absolute";
+    document.getElementById("navbar").style.background="var(--bg-color)";
+
+  }
 }
